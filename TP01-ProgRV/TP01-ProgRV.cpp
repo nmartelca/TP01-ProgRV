f220e206a9ec40e465c82718780f73fff434d102
nik_m <nik_m@DESKTOP-M7OUNCS>
2022-03-17 19:49:45 -0400

    Add project files.

---
+++ TP01-ProgRV/TP01-ProgRV.cpp
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+
+    const int MIN = 1;
+    const int MAX = 100;
+
+    srand(time(NULL));
+    int nbAleatoire = (rand() % (MAX - MIN + 1)) + MIN;
+    int nbALire = 0;
+
+    printf("quel est le nombre ? ");
+    scanf_s("%d", &nbALire);
+    while (nbALire != nbAleatoire) {
+
+        if (nbALire < nbAleatoire) {
+            printf("\nC' est plus !");
+        }
+        else {
+            printf("nC' est moins !");
+        }
+        printf("\nQuel est le nombre ? ");
+        scanf_s("%d", &nbALire);
+    }
+    printf("\nBravo, vous avez trouve le nombre mystere !!!");
+}
